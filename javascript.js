3b2dafe804660c8736044f37b5966ee679641d36
Dario_W <wasik.dariusz24@gmail.com>
2024-03-18 23:30:27 +0100

    add player punktation changing in playRound fnc

--- javascript.js
+++ javascript.js
@@ -3,46 +3,59 @@ let choices = ["rock" , "paper" , "scissors"];
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)]
 }
-// getComputerChoice();
-// console.log(getComputerChoice())
-// prompt window that allows player to chose paper rock or scissors 
-// let playerSelection = prompt("rock, paper or scissors?", "")
-// console.log(playerSelection);
 // function that compares player and computer choices 
+let playerPoint = 0;
+let computerPoint = 0;
+
 function playRound(playerSelection, computerSelection) {
     playerSelection =  prompt("rock, paper or scissors?", "");
     computerSelection = getComputerChoice();
     console.log(computerSelection);
-
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "rock") {
-        return "It's a tie"
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
-        return "You lose! paper beats rock"
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
-        return "You won! rock beats scissors"
     
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "rock") {
+        console.log("It's a tie");
+
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+        console.log ( "You lose! paper beats rock" );
+        return computerPoint = computerPoint + 1;
+        
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        console.log ("You won! rock beats scissors")
+        return playerPoint = playerPoint + 1;
+        
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
-        return "You lose! scissors beats paper"
+        console.log( "You lose! scissors beats paper")
+        return computerPoint = computerPoint + 1;
     }
-     else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper") {
-        return "It' a tie"
+    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper") {
+        console.log( "It' a tie");
     }
-     else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
-        return "You won! paper beats scissors"
+    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+        console.log("You won! paper beats scissors")
+        return playerPoint = playerPoint + 1;
     }
-     else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors") {
+    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors") {
         return "It's a tie"
     }
-     else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
-        return "You lose! rock beats scissors"
+    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+        console.log ( "You lose! rock beats scissors");
+        return computerPoint = computerPoint + 1;
     }
-     else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
-        return "You won! scissors beat paper"
+    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+        console.log("You won! scissors beat paper");
+        return playerPoint = playerPoint + 1;
     } else {
         return "Something's wrong"
     }
 }
 
-console.log (playRound())
+playRound();
+playRound();
+playRound();
 // function that allows play 5 round
+
+console.log(playerPoint);
+console.log(computerPoint) 
+
+
 // info about the game result
