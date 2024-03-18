86645c5dc9d153d74e30e11ce1ac9e92f2b3f78d
Dario_W <wasik.dariusz24@gmail.com>
2024-03-18 21:50:58 +0100

    write function computer choice

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
 // write function that gives randomly paper rock or scissors to the computer
+let choices = ["rock" , "paper" , "scissors"];
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)]
+}
+let computerChoice = getComputerChoice();
+console.log(computerChoice)
 // prompt window that allows player to chose paper rock or scissors 
+
 // function that compares player and computer choices 
 // function that allows play 5 round
 // info about the game result
