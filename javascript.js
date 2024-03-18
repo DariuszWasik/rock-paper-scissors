b8e0df6425335284d5f4efb0e98f4f05311fb234
Dario_W <wasik.dariusz24@gmail.com>
2024-03-18 21:27:38 +0100

    make plan for js in pseudocode

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// write function that gives randomly paper rock or scissors to the computer
+// prompt window that allows player to chose paper rock or scissors 
+// function that compares player and computer choices 
+// function that allows play 5 round
+// info about the game result
