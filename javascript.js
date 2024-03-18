a9908a5adef3571a20e815445f6844ebeb2efc61
Dario_W <wasik.dariusz24@gmail.com>
2024-03-18 21:56:36 +0100

    prompt asking player choice

--- javascript.js
+++ javascript.js
@@ -3,10 +3,11 @@ let choices = ["rock" , "paper" , "scissors"];
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)]
 }
-let computerChoice = getComputerChoice();
-console.log(computerChoice)
+let computerSelection = getComputerChoice();
+console.log(computerSelection)
 // prompt window that allows player to chose paper rock or scissors 
-
+let playerSelection = prompt("rock, paper or scissors?", "")
+console.log(playerSelection);
 // function that compares player and computer choices 
 // function that allows play 5 round
 // info about the game result
