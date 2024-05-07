41ab1201f766b3270c00634cd31a2cd5ee5cf7bf
Dario_W <wasik.dariusz24@gmail.com>
2024-05-07 13:35:23 +0200

    add buttons with eventlistener

--- javascript.js
+++ javascript.js
@@ -3,75 +3,95 @@ let choices = ["rock" , "paper" , "scissors"];
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)]
 }
+let playerSelection = ""
+
+
+//create buttons that allows player to choose his item
+const buttons = document.querySelectorAll("#buttons");
+console.log(buttons);
+const btn = document.querySelectorAll("#buttons > button");
+
+btn.forEach((i) => {
+    i.addEventListener("click", f => {
+        playerSelection = i.innerHTML;
+        console.log("player:", playerSelection);
+        return playerSelection
+    })
+    i.addEventListener("click", playRound)
+
+})
+
+
+
+
 // function that compares player and computer choices 
 let playerPoint = 0;
 let computerPoint = 0;
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection =  prompt("rock, paper or scissors?", "");
     computerSelection = getComputerChoice();
     console.log(computerSelection);
-    
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "rock") {
+    console.log(playerSelection, "z funkcji")
+    if (playerSelection = "rock" && computerSelection == "rock") {
         console.log("It's a tie");
 
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+    } else if (playerSelection = "rock" && computerSelection == "paper") {
         console.log ( "You lose! paper beats rock" );
         return computerPoint = computerPoint + 1;
         
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+    } else if (playerSelection = "rock" && computerSelection == "scissors") {
         console.log ("You won! rock beats scissors")
         return playerPoint = playerPoint + 1;
         
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+    } else if (playerSelection = "paper" && computerSelection == "scissors") {
         console.log( "You lose! scissors beats paper")
         return computerPoint = computerPoint + 1;
     }
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper") {
+    else if (playerSelection = "paper" && computerSelection == "paper") {
         console.log( "It' a tie");
     }
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+    else if (playerSelection = "paper" && computerSelection == "rock") {
         console.log("You won! paper beats scissors")
         return playerPoint = playerPoint + 1;
     }
-    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors") {
+    else if (playerSelection = "scissors" && computerSelection == "scissors") {
         return "It's a tie"
     }
-    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+    else if (playerSelection = "scissors" && computerSelection == "rock") {
         console.log ( "You lose! rock beats scissors");
         return computerPoint = computerPoint + 1;
     }
-    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+    else if (playerSelection = "scissors" && computerSelection == "paper") {
         console.log("You won! scissors beat paper");
         return playerPoint = playerPoint + 1;
     } else {
         console.log("Something's wrong, chose one more time!");
-        playRound();
+        ;
     }
 }
 
-// function that allows play n round
+// // function that allows play n round
 
-function playGame() {
+// function playGame() {
 
-    let n = prompt("how many games", 3);
-    for (let i = 1; i <= n; i++) {
-        playRound();
-    }
+//     let n = prompt("how many games", 3);
+//     for (let i = 1; i <= n; i++) {
+//         playRound();
+//     }
+ 
 
+//     // info about the game result
 
-    // info about the game result
-
-    console.log(`computer ${computerPoint} : player ${playerPoint} `);
+//     console.log(`computer ${computerPoint} : player ${playerPoint} `);
     
-    if (playerPoint > computerPoint) {
-        console.log('YOU WON CONGRATS');
-    } else { console.log("Not this time. Try again!"); }
-}
+//     if (playerPoint > computerPoint) {
+//         console.log('YOU WON CONGRATS');
+//     } else { console.log("Not this time. Try again!"); }
+// }
 
-    playGame();
+//     playGame();
 
 
-// console.log(playerPoint);
-// console.log(computerPoint) 
+// // console.log(playerPoint);
+// // console.log(computerPoint) 
 
